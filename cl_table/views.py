80e327bdacb9dcf2cf7394dd75c52c618e958d32
Pavithra Weerapperuma <pavithra.blog@gmail.com>
2021-06-03 21:59:57 +0530

    all schedule

--- cl_table/views.py
+++ cl_table/views.py
@@ -9508,7 +9508,7 @@ class MonthlyAllSchedule(APIView):
             # change to date range
             # start_date = datetime.datetime.strptime(request.GET.get("start"), "%Y-%m-%d").date()
             # end_date = datetime.datetime.strptime(request.GET.get("end"), "%Y-%m-%d").date()
-            date_range = [start_date + datetime.timedelta(days=i) for i in range(0,(end_date-start_date).days+1)]
+            date_range = [start_date + datetime.timedelta(days=i) for i in range(0,(end_date-start_date).days)]
         except Exception as e:
             print(e)
             return general_error_response("Invalid start and end date format")
@@ -9517,37 +9517,37 @@ class MonthlyAllSchedule(APIView):
         # if site_code hasn't in request, get month schedule by default site_code
         # site_code = request.GET.get("site_code",emp_obj.site_code)
 
-        try:
+        # try:
 
-            emp_qs = Employee.objects.filter(Site_Codeid=site)[0:10]
-            # todo: should be implement paginater or more filters to reduce qs lenght.
-            #  emp_qs = Employee.objects.filter(Site_Codeid=site)[0:10]
-            emp_schedule_list = []
-            for emp in emp_qs:
-                date_list = []
+        emp_qs = Employee.objects.filter(Site_Codeid=site,emp_isactive=True)[:5]
+        # todo: should be implement paginater or more filters to reduce qs lenght.
+        #  emp_qs = Employee.objects.filter(Site_Codeid=site)[0:10]
+        emp_schedule_list = []
+        for emp in emp_qs:
+            date_list = []
 
-                for date in date_range:
-                    month_schedule = ScheduleMonth.objects.filter(emp_code=emp.emp_code,
-                                                                  site_code=site.itemsite_code,
-                                                                  itm_date=date,
-                                                                  ).first()
-                    if not month_schedule:
-                        month_schedule = ScheduleMonth.objects.create(emp_code=request.GET.get("emp_code"),
-                                                                      site_code=site.itemsite_code,
-                                                                      itm_date=date, )
+            for date in date_range:
+                month_schedule = ScheduleMonth.objects.filter(emp_code=emp.emp_code,
+                                                              site_code=site.itemsite_code,
+                                                              itm_date=date,
+                                                              ).first()
+                # if not month_schedule:
+                #     month_schedule = ScheduleMonth.objects.create(emp_code=emp.emp_code,
+                #                                                   site_code=site.itemsite_code,
+                #                                                   itm_date=date, )
 
-                    date_list.append({
-                        "id": month_schedule.id,
-                        "itm_date": month_schedule.itm_date,
-                        "itm_type": month_schedule.itm_type,
-                    })
-                emp_schedule_list.append({
-                    "emp_name": emp.emp_name,
-                    "emp_code": emp.emp_code,
-                    "schedules": date_list
+                date_list.append({
+                    "id": month_schedule.id if month_schedule else None,
+                    "date": date,
+                    "itm_type": month_schedule.itm_type if month_schedule else None,
                 })
-        except Exception as e:
-            return general_error_response(e)
+            emp_schedule_list.append({
+                "emp_name": emp.emp_name,
+                "emp_code": emp.emp_code,
+                "schedules": date_list
+            })
+        # except Exception as e:
+        #     return general_error_response(e)
 
         resData = {
             "fullSchedule": emp_schedule_list,
